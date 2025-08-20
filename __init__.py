69aac977972bcba1b0328b5092a3d370444ab916
WillDyer <will.dyer9@gmail.com>
2025-08-20 19:05:33 +0100

    feat: logo

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 from PySide6.QtWidgets import QApplication
-import sys
+from PySide6.QtGui import QIcon
+import sys, os
 import importlib
 import base.base as base
 importlib.reload(base)
 
 app = QApplication.instance() or QApplication(sys.argv)
+app.setWindowIcon(QIcon(os.path.join(os.path.dirname(os.path.abspath(__file__)),"icons","autorig.svg")))
 window = base.NodeEditorWindow()
 window.show()
 app.exec()
