ada0388e6a57913e2fc8d6ee31e0d390e9bc082c
WillDyer <will.dyer9@gmail.com>
2025-08-24 17:06:21 +0100

    feat: clearing item focusing

--- base/utils/text_utils.py
+++ base/utils/text_utils.py
@@ -1,3 +1,6 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QLayout
+
 def sanitise_text(node_text=None):
             doc = node_text.document()
             current_text = doc.toPlainText()
@@ -6,3 +9,24 @@ def sanitise_text(node_text=None):
                 doc.blockSignals(True)
                 doc.setPlainText(new_text)
                 doc.blockSignals(False)
+
+
+def disable_tab_focus(obj):
+    if isinstance(obj, QWidget):
+        obj.setFocusPolicy(Qt.NoFocus)
+        for child in obj.findChildren(QWidget):
+            child.setFocusPolicy(Qt.NoFocus)
+
+    elif isinstance(obj, QLayout):
+        for i in range(obj.count()):
+            item = obj.itemAt(i)
+
+            widget = item.widget()
+            if widget:
+                widget.setFocusPolicy(Qt.NoFocus)
+                for child in widget.findChildren(QWidget):
+                    child.setFocusPolicy(Qt.NoFocus)
+
+            layout = item.layout()
+            if layout:
+                disable_tab_focus(layout)
